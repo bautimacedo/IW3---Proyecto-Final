cf9f88c6650e300c076c0fcdaf373488cb30431a
bautimacedo <bautimrf@gmail.com>
2025-10-31 16:26:36 -0300

    EstadoOrden agregado, OrdenDeserializer planteado

---
+++ src/main/java/project/iw3/iw3/integracion/cli1/model/OrdenDeserializer.java
@@ -0,0 +1,2 @@
+//TODO
+//No se puede hacer hasta que este la entidad Orden.
---
+++ src/main/java/project/iw3/iw3/model/enums/EstadoOrden.java
@@ -0,0 +1,8 @@
+package project.iw3.iw3.model.enums;
+
+public enum EstadoOrden {
+    PENDIENTE_PESAJE_INICIAL,
+    CON_PESAJE_INICIAL,
+    CERRADA_PARA_CARGA,
+    FINALIZADA
+}
