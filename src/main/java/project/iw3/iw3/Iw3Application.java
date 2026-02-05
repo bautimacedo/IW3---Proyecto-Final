e167d45c62c7167724c85f831036b3add43c5fd3
bautimacedo <bautimrf@gmail.com>
2026-02-05 19:51:46 -0300

    segundo intento fix deploy

--- src/main/java/project/iw3/iw3/Iw3Application.java
+++ src/main/java/project/iw3/iw3/Iw3Application.java
@@ -2,12 +2,19 @@ package project.iw3.iw3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Iw3Application {
+public class Iw3Application extends SpringBootServletInitializer { // <--- 1. Extender esta clase
 
-	public static void main(String[] args) {
-		SpringApplication.run(Iw3Application.class, args);
-	}
+    // 2. Sobrescribir este método para configurar la aplicación en el servidor
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Iw3Application.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(Iw3Application.class, args);
+    }
 }
