193e4ceb797f62fd9a669a51ea49c2620f3b0eb1
Agustin Rodeyro <rodeyroagustin@gmail.com>
2026-02-09 20:29:30 -0300

    changes

--- src/main/java/project/iw3/iw3/controllers/AlarmRestController.java
+++ src/main/java/project/iw3/iw3/controllers/AlarmRestController.java
@@ -1,10 +1,13 @@
 package project.iw3.iw3.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import lombok.extern.slf4j.Slf4j;
+import project.iw3.iw3.model.business.exceptions.BusinessException;
 import project.iw3.iw3.model.business.interfaces.IAlarmBusiness;
 
 @Slf4j
@@ -16,12 +19,14 @@ public class AlarmRestController {
     private IAlarmBusiness alarmBusiness;
 
     @GetMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
     public ResponseEntity<?> list() {
         try {
             return ResponseEntity.ok(alarmBusiness.list());
-        } catch (Exception e) {
-            log.error("Error listando alarmas", e);
-            return ResponseEntity.internalServerError().body("Error interno");
+        } catch (BusinessException e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(e.getMessage());
         }
-    }
+    }   
 }
